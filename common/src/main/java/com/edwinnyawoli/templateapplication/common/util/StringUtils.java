00fc91e29c53c3e7f4d698b0af02b42e1ec08d23
Edwin Nyawoli <edwin.nyawoli@gmail.com>
2019-06-13 10:15:08 +0000

    Add methods to utility class to check if string has html elements

--- common/src/main/java/com/edwinnyawoli/templateapplication/common/util/StringUtils.java
+++ common/src/main/java/com/edwinnyawoli/templateapplication/common/util/StringUtils.java
@@ -19,11 +19,15 @@ package com.edwinnyawoli.templateapplication.common.util;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  */
 public class StringUtils {
+    private static Pattern pattern = Pattern.compile("<(\"[^\"]*\"|'[^']*'|[^'\">])*>");
+
     /**
      * @param value
      * @return An empty string if value is null and the value itself if not null.
@@ -64,5 +68,36 @@ public class StringUtils {
             return original.substring(0, 1).toUpperCase()
                     + original.substring(1).toLowerCase();
         }
+    }
+
+    public static boolean hasHTMLTags(String string) {
+        Matcher matcher = pattern.matcher(string);
+        return matcher.find();
+    }
+
+    /** Trims trailing whitespace. Removes any of these characters:
+     * 0009, HORIZONTAL TABULATION
+     * 000A, LINE FEED
+     * 000B, VERTICAL TABULATION
+     * 000C, FORM FEED
+     * 000D, CARRIAGE RETURN
+     * 001C, FILE SEPARATOR
+     * 001D, GROUP SEPARATOR
+     * 001E, RECORD SEPARATOR
+     * 001F, UNIT SEPARATOR
+     * @return "" if source is null, otherwise string with all trailing whitespace removed
+     */
+    public static CharSequence trimTrailingWhitespace(CharSequence source) {
+
+        if(source == null)
+            return "";
+
+        int i = source.length();
+
+        // loop back to the first non-whitespace character
+        while(--i >= 0 && Character.isWhitespace(source.charAt(i))) {
+        }
+
+        return source.subSequence(0, i+1);
     }
 }
