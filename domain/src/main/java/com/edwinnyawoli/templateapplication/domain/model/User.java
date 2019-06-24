dbd21eae4edb14615fd7c8aed384b502ff091c0a
Edwin Nyawoli <edwin.nyawoli@gmail.com>
2019-06-24 15:09:49 +0000

    Fix pmd complaints of unused variables in domain module

--- domain/src/main/java/com/edwinnyawoli/templateapplication/domain/model/User.java
+++ domain/src/main/java/com/edwinnyawoli/templateapplication/domain/model/User.java
@@ -1,9 +1,12 @@
 package com.edwinnyawoli.templateapplication.domain.model;
 
+import lombok.Value;
+
 /**
  *
  */
 
+@Value
 public class User {
     private String username;
     private String password;
